42064bcc214668cc5b38213bffe1bf2ae49755d5
Elio <eliocamison@gmail.com>
2018-01-24 23:01:08 +0100

    Restructurando las clases

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import com.elioCamison.bigNumber.BigNumber;
 public class Main {
 
     public static void main(String[] args) {
-        BigNumber n1 = new BigNumber("3");
-        BigNumber n2 = new BigNumber("3");
+        BigNumber n1 = new BigNumber("2");
+        BigNumber n2 = new BigNumber("02");
 
         //n1.compareTo(n2);
 
