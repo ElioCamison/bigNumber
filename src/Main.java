6db76be4be12b3c97f83d1cf6e74399b1d2389c2
Elio <eliocamison@gmail.com>
2018-01-27 15:44:52 +0100

    Flipo con el Ctrl + z .... -_-

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,22 @@ import com.elioCamison.bigNumber.BigNumber;
 public class Main {
 
     public static void main(String[] args) {
-        BigNumber n1 = new BigNumber("00022");
-        BigNumber n2 = new BigNumber("090");
-
-        /*String aux = n1.getNumero();
-
-        for (int i = 0; i < aux.length(); i++) {
-            System.out.println("Resultat de i " + i);
-
-            if (i == aux.length()-1){
-                System.out.println("hooooola");
-            }
-        }
+        BigNumber n1 = new BigNumber("34535233");
+        BigNumber n2 = new BigNumber("04533453");
 
 
-        System.out.println(aux.length());*/
+
+        /*String aux1 = n1.getNumero();
+        String aux2 = n2.getNumero();
+
+        int cont =aux1.length() - aux2.length();
+        System.out.println(cont);*/
+
+        //System.out.println(aux.length());
 
         //n1.compareTo(n2);
 
-        System.out.println(n1.add(n2));
+        System.out.println(n1.sub(n2));
         System.out.println(n1.compareTo(n2));
 
 
